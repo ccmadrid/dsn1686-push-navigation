24a7baf134229cb07b7ab639b41d4e90b6689f05
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2016-02-08 17:50:23 -0500

    feat(css): add sub level navigation

--- js/push-menu.js
+++ js/push-menu.js
@@ -17,26 +17,16 @@ function toggleNav(){
     $('.site-wrapper').attr('data-state', 'slide-closed');
   }
 }
+$(function(){
+  $('.subsites').toggleClass('hidden');
+});
+
+$(function(){
+  $('.subsite-parent').click(function(){
+    $('.subsites').toggleClass('hidden');
+  });
+});
 
 
 
-
-
-// $(function() {
-//   $('body').removeClass('noscript');
-
-//   $('.toggle-btn, .close-btn').click(function() {
-//     toggleNav();
-//   });
-// });
-
-// function toggleNav() {
-//   if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
-//     // Display Nav when closed
-//     $('.site-wrapper').attr('data-state', 'slide-open');
-//   } else {
-//     // Hide Nav when open
-//     $('.site-wrapper').attr('data-state', 'slide-closed');
-//   }
-// }
 
